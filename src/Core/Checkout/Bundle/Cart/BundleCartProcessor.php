059c3c640015a0cbbdc7a8bb8f27cf1397306502
Oliver Skroblin <o.skroblin@shopware.com>
2019-06-28 11:20:49 +0200

    Added inline documentation

--- src/Core/Checkout/Bundle/Cart/BundleCartProcessor.php
+++ src/Core/Checkout/Bundle/Cart/BundleCartProcessor.php
@@ -28,7 +28,7 @@ class BundleCartProcessor implements CartProcessorInterface, CartDataCollectorIn
 {
     public const TYPE = 'swagbundle';
     public const DISCOUNT_TYPE = 'swagbundle-discount';
-    private const DATA_KEY = 'swag_bundle-';
+    public const DATA_KEY = 'swag_bundle-';
     public const DISCOUNT_TYPE_ABSOLUTE = 'absolute';
     public const DISCOUNT_TYPE_PERCENTAGE = 'percentage';
 
@@ -68,28 +68,37 @@ class BundleCartProcessor implements CartProcessorInterface, CartDataCollectorIn
         $bundleLineItems = $original->getLineItems()
             ->filterType(self::TYPE);
 
+        // no bundles in cart? exit
         if (\count($bundleLineItems) === 0) {
             return;
         }
 
+        // fetch missing bundle information from database
         $bundles = $this->fetchBundles($bundleLineItems, $data, $context);
 
         /** @var BundleEntity $bundle */
         foreach ($bundles as $bundle) {
+            // ensure all line items have a data entry
             $data->set(self::DATA_KEY . $bundle->getId(), $bundle);
         }
 
         foreach ($bundleLineItems as $bundleLineItem) {
             $bundle = $data->get(self::DATA_KEY . $bundleLineItem->getReferencedId());
 
+            // enrich bundle information with quantity and delivery information
             $this->enrichBundle($bundleLineItem, $bundle);
+
+            // add bundle products which are not already assigned
             $this->addMissingProducts($bundleLineItem, $bundle);
+
+            // add bundle discount if not already assigned
             $this->addDiscount($bundleLineItem, $bundle, $context);
         }
     }
 
     public function process(CartDataCollection $data, Cart $original, Cart $toCalculate, SalesChannelContext $context, CartBehavior $behavior): void
     {
+        // collect all bundle in cart
         $bundleLineItems = $original->getLineItems()
             ->filterType(self::TYPE);
 
@@ -98,10 +107,18 @@ class BundleCartProcessor implements CartProcessorInterface, CartDataCollectorIn
         }
 
         foreach ($bundleLineItems as $bundleLineItem) {
+            // first calculate all bundle product prices
             $this->calculateChildProductPrices($bundleLineItem, $context);
-            $this->calculateDiscountPrice($bundleLineItem, $context);
-            $this->calculateBundlePrice($bundleLineItem);
 
+            // after the product prices calculated, we can calculate the discount
+            $this->calculateDiscountPrice($bundleLineItem, $context);
+
+            // at last we have to set the total price for the root line item (the bundle)
+            $bundleLineItem->setPrice(
+                $bundleLineItem->getChildren()->getPrices()->sum()
+            );
+
+            // afterwards we can move the bundle to the new cart
             $toCalculate->add($bundleLineItem);
         }
     }
@@ -161,7 +178,6 @@ class BundleCartProcessor implements CartProcessorInterface, CartDataCollectorIn
 
             // the ProductCartProcessor will enrich the product further
             $productLineItem = new LineItem($productId, LineItem::PRODUCT_LINE_ITEM_TYPE, $productId);
-            $productLineItem->setPayload(['id' => $productId]);
 
             $bundleLineItem->addChild($productLineItem);
         }
@@ -169,12 +185,14 @@ class BundleCartProcessor implements CartProcessorInterface, CartDataCollectorIn
 
     private function addDiscount(LineItem $bundleLineItem, BundleEntity $bundle, SalesChannelContext $context): void
     {
-        if (!$this->getDiscount($bundleLineItem)) {
-            $discount = $this->createDiscount($bundle, $context);
+        if ($this->getDiscount($bundleLineItem)) {
+            return;
+        }
 
-            if ($discount) {
-                $bundleLineItem->addChild($discount);
-            }
+        $discount = $this->createDiscount($bundle, $context);
+
+        if ($discount) {
+            $bundleLineItem->addChild($discount);
         }
     }
 
@@ -219,11 +237,15 @@ class BundleCartProcessor implements CartProcessorInterface, CartDataCollectorIn
 
     private function calculateChildProductPrices(LineItem $bundleLineItem, SalesChannelContext $context): void
     {
-        foreach ($bundleLineItem->getChildren()->filterType(LineItem::PRODUCT_LINE_ITEM_TYPE) as $lineItem) {
-            /** @var QuantityPriceDefinition $priceDefinition */
-            $priceDefinition = $lineItem->getPriceDefinition();
+        $products = $bundleLineItem->getChildren()->filterType(LineItem::PRODUCT_LINE_ITEM_TYPE);
 
-            $lineItem->setPrice($this->quantityPriceCalculator->calculate($priceDefinition, $context));
+        foreach ($products as $product) {
+            /** @var QuantityPriceDefinition $priceDefinition */
+            $priceDefinition = $product->getPriceDefinition();
+
+            $product->setPrice(
+                $this->quantityPriceCalculator->calculate($priceDefinition, $context)
+            );
         }
     }
 
@@ -268,12 +290,5 @@ class BundleCartProcessor implements CartProcessorInterface, CartDataCollectorIn
         }
 
         $discount->setPrice($price);
-    }
-
-    private function calculateBundlePrice(LineItem $bundleLineItem): void
-    {
-        $bundleLineItem->setPrice(
-            $bundleLineItem->getChildren()->getPrices()->sum()
-        );
     }
 }
