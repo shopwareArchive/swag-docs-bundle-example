b05175aa1d39df7f2a5bcb4ed4bced138909c697
Jonas Elfering <jonas.elfering@web.de>
2019-10-07 16:06:17 +0200

    NTR - Fix handling of inherited bundles in admin

--- src/Resources/administration/extension/sw-product/view/sw-product-detail-base/index.js
+++ src/Resources/administration/extension/sw-product/view/sw-product-detail-base/index.js
@@ -2,5 +2,20 @@ import { Component } from 'src/core/shopware';
 import template from './sw-product-detail-base.html.twig';
 
 Component.override('sw-product-detail-base', {
-    template
+    template,
+
+    computed: {
+        productRepository() {
+            return this.repositoryFactory.create('product');
+        },
+    },
+
+    methods: {
+        saveProduct() {
+            if (this.product) {
+                this.productRepository.save(this.product, this.context);
+            }
+        }
+    }
+
 });
