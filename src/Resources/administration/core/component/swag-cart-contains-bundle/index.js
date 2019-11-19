51a686dd32540641e70aa59d08da30bdad9b3998
Jonas Elfering <jonas.elfering@web.de>
2019-11-19 15:44:51 +0100

    NTR - Remove old administration code

--- src/Resources/administration/core/component/swag-cart-contains-bundle/index.js
+++
@@ -1,7 +0,0 @@
-import template from './swag-cart-contains-bundle.tml.twig';
-
-const { Component } = Shopware;
-
-Component.extend('swag-cart-contains-bundle', 'sw-condition-base', {
-    template
-});
--- src/Resources/administration/main.js
+++
@@ -1,6 +0,0 @@
-import './module/swag-bundle';
-
-import './extension/sw-product/view/sw-product-detail-base';
-import './extension/sw-product/page/sw-product-detail';
-
-import './decorator/rule-condition-service-decoration';
